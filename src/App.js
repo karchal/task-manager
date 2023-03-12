876101af11d1cda4f2c95429b462625ca2d6885d
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-12 13:20:11 +0100

    create simple header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import Header from './components/Header'
+
 function App() {
-    const name = 'Karolina'
-    const x = true
+
   return (
     <div className="container">
-        <h1>Hello</h1>
-        <h2>Hello {name}</h2>
-        <h2>Hello {x ? 'yes' : 'no'}</h2>
+        <Header/>
     </div>
   );
 }
