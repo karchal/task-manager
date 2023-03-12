a23eebcf680266e11cd4cc294f10c9a225e00a8e
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-12 12:43:05 +0100

    use variables in App() function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
+    const name = 'Karolina'
+    const x = true
   return (
     <div className="container">
-      <h1>Hello</h1>
+        <h1>Hello</h1>
+        <h2>Hello {name}</h2>
+        <h2>Hello {x ? 'yes' : 'no'}</h2>
     </div>
   );
 }
