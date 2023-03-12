387be2047a85b2bcb3e0818431d6cc1372fdce18
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-12 14:26:04 +0100

    create stupid event on click

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = ({color, text}) => {
+    const onClick = () => {
+        console.log('click')
+    }
     return (
         <div>
-            <button style={{backgroundColor: color}} className='btn'>{text}</button>
+            <button onClick={onClick}
+                    style={{backgroundColor: color}}
+                    className='btn'>{text}</button>
         </div>
     );
 };
