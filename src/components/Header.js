87c63f13fc1f9defd71f133a9a964eea7fea8c24
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-12 13:31:25 +0100

    use prop in header, style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = ({ title }) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1 style={{ color: 'darkblue', backgroundColor: 'bisque'}}>{ title }</h1>
         </header>
     );
-};
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
 
 export default Header;
