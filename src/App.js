73653a1e0b00f49631965a840790257ad2927005
Karolina Chalupka <k.s.chalupka@gmail.com>
2023-03-12 17:54:07 +0100

    create Tasks component and show some exemplary tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import Header from './components/Header'
+import Tasks from "./components/Tasks";
+import {useState} from "react";
 
 function App() {
-
-  return (
-    <div className="container">
-        <Header/>
-    </div>
-  );
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2:30pm',
+            remainder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeteng at school',
+            day: 'Feb 6th at 1:30pm',
+            remainder: true,
+        },
+        {
+            id: 3,
+            text: 'Food shopping',
+            day: 'Feb 5th at 2:30pm',
+            remainder: false,
+        },
+    ])
+    return (
+        <div className="container">
+            <Header/>
+            <Tasks tasks={tasks}/>
+        </div>
+    );
 }
 
 export default App;
